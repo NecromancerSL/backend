c7ab55d2a81e9530ffe33b5864125285f05d65e8
Gustavo Nunes <dsnunes.g42@outlook.com>
2023-09-18 16:53:59 -0300

    adicionando funçoes de controller do produto e suas rotas

--- src/controllers/produtoController.ts
+++ src/controllers/produtoController.ts
@@ -32,5 +32,66 @@ export const listarProdutos = async (req: Request, res: Response) => {
     }
 };
 
+export const atualizarProduto = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { nome, descricao, categoria, preco, imagem, qntEstoque } = req.body;
 
+    if (!nome || !descricao || !categoria || !preco || !imagem || !qntEstoque) {
+        return res.status(400).json({ message: 'Todos os campos são obrigatórios' });
+      }
 
+    try {
+        const produto = await Produto.findByPk(id);
+
+        if (!produto) {
+            return res.status(400).json({ message: 'Produto não encontrado' });
+        }
+
+        await produto.update({
+            nome,
+            descricao,
+            categoria,
+            preco,
+            imagem,
+            qntEstoque,
+        });
+
+        return res.json(produto);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+};
+
+export const listarProduto = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const produto = await Produto.findByPk(id);
+
+        if (!produto) {
+            return res.status(400).json({ message: 'Produto não encontrado' });
+        }
+
+        return res.json(produto);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+};
+
+export const deletarProduto = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const produto = await Produto.findByPk(id);
+
+        if (!produto) {
+            return res.status(400).json({ message: 'Produto não encontrado' });
+        }
+
+        await produto.destroy();
+
+        return res.json({ message: 'Produto deletado com sucesso' });
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+};
--- src/routes/produto/index.ts
+++ src/routes/produto/index.ts
@@ -9,5 +9,9 @@ produtoRoutes.use(bodyParser.json());
 
 produtoRoutes.post('/criarproduto', ProdutoController.cadastrarProduto);
 produtoRoutes.get('/listarprodutos', ProdutoController.listarProdutos);
+produtoRoutes.post('/atualizarproduto/:id', ProdutoController.atualizarProduto);
+produtoRoutes.get('/listarproduto/:id', ProdutoController.listarProduto);
+produtoRoutes.delete('/deletarproduto/:id', ProdutoController.deletarProduto);
+
 
 export default produtoRoutes;
