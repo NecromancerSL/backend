be2ed49af9a38b02b95cb121ed72f0b89e3451a0
Gustavo Nunes <dsnunes.g42@outlook.com>
2023-11-14 11:22:57 -0300

    apagando comentarios

--- src/services/produto/ListProdutoByNameService.ts
+++ src/services/produto/ListProdutoByNameService.ts
@@ -8,17 +8,15 @@ export class ListProdutoByNameService {
       const produtos = await Produto.findAll({
         where: {
           nome: {
-            [Op.iLike]: `%${nome}%`, // Use Op.iLike for case-insensitive partial match
+            [Op.iLike]: `%${nome}%`,
           },
         },
       });
 
-      // Assuming IProdutoData is the shape of your data, you might need to map it
       const produtosData: IProdutoData[] = produtos.map((produto) => produto.toJSON());
 
       return produtosData;
     } catch (error) {
-      // Handle errors, log, or throw an exception
       console.error("Error fetching products by name:", error);
       throw error;
     }
